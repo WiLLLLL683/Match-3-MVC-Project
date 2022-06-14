28c7f9eef3499022f757d67e6d131141aa54c12a
WiLLLLL683 <32411970+WiLLLLL683@users.noreply.github.com>
2022-06-14 20:47:15 +0800

    Конструктор блока, заготовка тестов блока, фиксы игровой доски

--- Assets/Scripts/Model/Objects/Block.cs
+++ Assets/Scripts/Model/Objects/Block.cs
@@ -7,16 +7,16 @@ namespace Model.Objects
     public class Block
     {
         public IBlockType type { get; private set; }
-        private Vector2Int position;
+        public Vector2Int position { get; private set; }
 
         public Block(IBlockType _type, Vector2Int _position)
         {
             type = _type;
             position = _position;
-            //TODO загрузка параметров
+            //TODO пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
         }
 
-        public void SetPosition(Vector2Int _position) //TODO дублирование данных о положении - может быть стоит дать ссылку на клетку?
+        public void SetPosition(Vector2Int _position) //TODO пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ - пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ?
         {
             position = _position;
         }
---
+++ Assets/Tests/EditorTests/BlockTests.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class BlockTests
+{
+    [Test]
+    public void SetPosition_InsideBoard_True()
+    {
+
+    }
+
+    public void SetPosition_OutsideBoard_False()
+    {
+
+    }
+
+    public void ChangeType_NewType_TypeEqNewType()
+    {
+
+    }
+    public void Activate_CurrentType_Activated()
+    {
+
+    }
+
+    public void Activate_NewType_ActivatedNewType()
+    {
+
+    }
+
+    public void Destroy_Block_CellEmpty()
+    {
+
+    }
+
+    public void Destroy_Block_DestroyEvent()
+    {
+
+    }
+}
