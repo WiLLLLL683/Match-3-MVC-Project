32520dab327cd215b3e5a79afc2e95ec70c70480
WiLLLLL683 <32411970+WiLLLLL683@users.noreply.github.com>
2023-01-04 18:38:54 +0800

    Базовый инвентарь

---
+++ Assets/Scripts/Model/Objects/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    public int Gold { get; private set; }
+
+    public void AddGold(int ammount)
+    {
+        if (ammount > 0)
+        {
+            Gold += ammount;
+        }
+        else
+        {
+            Debug.LogError("Can't add negative ammount of Gold");
+        }
+    }
+
+    public void RemoveGold(int ammount)
+    {
+        if (ammount > 0)
+        {
+            Gold -= ammount;
+        }
+        else
+        {
+            Debug.LogError("Can't remove negative ammount of Gold");
+        }
+    }
+}
