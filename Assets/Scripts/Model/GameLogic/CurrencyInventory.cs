d57ddb2ea08440d4d7ea11581c946526482c193c
WiLLLLL683 <32411970+WiLLLLL683@users.noreply.github.com>
2023-01-06 18:46:16 +0800

    Разделение инвентаря валют и бустеров

---
+++ Assets/Scripts/Model/GameLogic/CurrencyInventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.GameLogic
+{
+    public class CurrencyInventory
+    {
+        public int Gold { get; private set; }
+
+        public void AddGold(int ammount)
+        {
+            if (ammount <= 0)
+            {
+                Debug.LogError("Can't add negative ammount of Gold");
+                return;
+            }
+
+            Gold += ammount;
+        }
+
+        public void TakeGold(int ammount)
+        {
+            if (ammount <= 0)
+            {
+                Debug.LogError("Can't remove negative ammount of Gold");
+                return;
+            }
+
+            if (ammount > Gold)
+            {
+                Debug.LogError("Not enough Gold");
+                return;
+            }
+
+            Gold -= ammount;
+        }
+    }
+}
